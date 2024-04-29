33381a7fabf6cd548646d5e4ecfb6b34d4455362
Hiroshi Miura <miurahr@linux.com>
2024-04-29 16:05:34 +0900

    chore: drop module-info for junit/jupiter modules

--- assertj-swing-junit-jupiter/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.assertj.swing.junit.jupiter {
-  requires org.assertj.core;
-  requires org.assertj.swing.junit;
-  requires org.junit.jupiter.api;
-
-  exports org.assertj.swing.junit.extension;
-}
